c834169cb010ed1f3c52b2a7900c26e86042b53f
iskander brahim <iskander.skan05@yahoo.com>
2026-01-04 21:43:55 +0100

    test

--- backend/src/main/java/com/opulune/opulune/service/EmailService.java
+++ backend/src/main/java/com/opulune/opulune/service/EmailService.java
@@ -10,7 +10,7 @@ public class EmailService {
 
     @Autowired
     private JavaMailSender mailSender;
-        private final String from = "iskanderbrahim2024@gmail.com";
+        private final String from = "9f4310001@smtp-brevo.com";
 
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
